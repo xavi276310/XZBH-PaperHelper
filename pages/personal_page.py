4b3714d27a17c629431d78764992c7df9f033233
xavi276310 <xavi276310@gmail.com>
2024-12-07 19:57:00 +0800

    new commit

--- pages/personal_page.py
+++ pages/personal_page.py
@@ -9,4 +9,9 @@ def show_personal_page():
         """这是我的个人空间，未来我会逐步推出一些新的功能和内容。你可以通过点击链接访问我的网页，我致力于分享深度学习写论文的相关模块和教程。所有的视频内容都旨在为你提供新的学习机会，让你在观看的过程中收获实用的知识。无论是专业技能、兴趣爱好，还是个人成长方面的知识，我希望能够为大家带来启发和帮助。即便是没有购买课程也必能有所收获""")
 
     # 添加B站链接
-    st.markdown("[🎬 访问我的B站空间](https://space.bilibili.com/478113245)", unsafe_allow_html=True) 
+    st.markdown("[🎬 访问我的B站空间](https://space.bilibili.com/478113245)", unsafe_allow_html=True)
+    
+    # 添加模块缝合视频链接
+    st.markdown("这是我的其中两个有关模块缝合的视频链接：")
+    st.markdown("- [模块缝合视频 1](https://www.bilibili.com/video/BV1zg4y1C7oP/)")
+    st.markdown("- [模块缝合视频 2](https://www.bilibili.com/video/BV1Kw411G7Rw)")
